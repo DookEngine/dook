d6de47c15afa3c180c4d1e74355e58228efa50e5
ambertide <egeemirozkan24@gmail.com>
2024-02-13 08:16:40 +0300

    feat(entities): add level constructor, dest

--- src/entities/include/Level.hpp
+++ src/entities/include/Level.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+#include <string>
 #include <Texture.hpp>
 #include <Entity.hpp>
 namespace dook
@@ -14,8 +16,50 @@ namespace dook
     class Level : public Entity
     {
     private:
-        Texture<T> background;
-        Texture<T> foreground;
-        Texture<T> passability;
+        std::unique_ptr<Texture<T>> background;
+        std::unique_ptr<Texture<T>> foreground;
+        std::unique_ptr<Texture<T>> passability;
+
+    public:
+        /**
+         * @brief Construct a new Level object
+         *
+         * @param name Entity name of the level.
+         * @param background Background texture that will be rendered
+         * before the characters.
+         * @param foreground Foreground texture that will be rendered
+         * in front of the characters.
+         * @param passability Passability layer that decides which tiles
+         * are passable.
+         * @param source Information about source file.
+         */
+        Level(
+            std::string name,
+            std::unqiue_ptr<Texture<T>> background,
+            std::unqiue_ptr<Texture<T>> foreground,
+            std::unqiue_ptr<Texture<T>> passability,
+            SourceInfo source)
+            : Entity(name, EntityType::LEVEL, source),
+              background(background),
+              foreground(foreground),
+              passability(passability) {}
+        /**
+         * @brief Construct a new Level object
+         *
+         * @param name Entity name of the level.
+         * @param background Background texture that will be rendered
+         * before the characters.
+         * @param foreground Foreground texture that will be rendered
+         * in front of the characters.
+         * @param passability Passability layer that decides which tiles
+         * are passable.
+         */
+        Level(
+            std::string name,
+            std::unqiue_ptr<Texture<T>> background,
+            std::unqiue_ptr<Texture<T>> foreground,
+            std::unqiue_ptr<Texture<T>> passability)
+            : Level(name, background, foreground, passability);
+        ~Level() = default;
     };
 };
