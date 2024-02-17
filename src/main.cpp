54d232570d40b88ffa87fccee1efab1230e1f8d0
Ege Özkan <egeemirozkan24@gmail.com>
2024-02-17 23:36:03 +0300

    fix(main): use concrete classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Rect.hpp>
 #include <iostream>
 #include <Level.hpp>
-#include <Texture.hpp>
-#include <Audio.hpp>
+#include <NullTexture.hpp>
+#include <NullAudio.hpp>
 
 int main()
 {
-    auto bg = std::make_unique<dook::Texture>(std::string{"file.png"});
-    auto fg = std::make_unique<dook::Texture>(std::string{"file.png"});
-    auto ps = std::make_unique<dook::Texture>(std::string{"file.png"});
+    auto bg = std::make_unique<dook::NullTexture>(std::string{"file.png"});
+    auto fg = std::make_unique<dook::NullTexture>(std::string{"file.png"});
+    auto ps = std::make_unique<dook::NullTexture>(std::string{"file.png"});
     // Generate empty level.
     dook::Level level(
         std::string{"Sample"},
