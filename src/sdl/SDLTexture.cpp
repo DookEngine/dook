4ba47ef6f713bc418896a478329b48bc7141075c
ambertide <egeemirozkan24@gmail.com>
2024-02-27 08:57:43 +0300

    feat(sdl): add texture loading from filesystem.

--- src/sdl/SDLTexture.cpp
+++ src/sdl/SDLTexture.cpp
@@ -9,3 +9,19 @@ dook::Rect dook::SDLTexture::texture_size() const
 {
     return this->_texture_size;
 }
+
+dook::SDLTexture::~SDLTexture()
+{
+    SDL_DestroyTexture(this->_texture);
+    SDL_FreeSurface(this->_surface);
+}
+
+SDL_Texture *&dook::SDLTexture::texture()
+{
+    return this->_texture;
+}
+
+SDL_Surface *&dook::SDLTexture::surface()
+{
+    return this->_surface;
+}
