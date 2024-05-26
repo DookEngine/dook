0faa014e437a866864e4ba9a728eb0c22f3ad88c
Ege Özkan <egeemirozkan24@gmail.com>
2024-05-26 23:47:22 +0300

    chore(docs): fix doxyfile path for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 breathe_projects = {}
 
 if read_the_docs_build:
-    subprocess.call('doxygen ../Doxyfile', shell=True)
+    subprocess.call('doxygen Doxyfile', shell=True, cwd="../")
     breathe_projects['Dook'] =  '../xml'
 
 
